cb6a9524b41f4d28a519f676cf3bf7839258ca46
MarkovR <markovkot@gmail.com>
2022-09-22 21:18:52 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go func() {
+		for i := 0; i < 50; i++ {
+			ch1 <- i
+		}
+		//close(ch1)
+	}()
+	go func() {
+		for i := 0; i < 50; i++ {
+			ch2 <- i + 50
+		}
+		//close(ch2)
+	}()
+	for {
+		select {
+		case <-ch1:
+			fmt.Println("In 1 chan")
+		case <-ch2:
+			fmt.Println("In 2 chan")
+		default:
+			fmt.Println(time.Now())
+		}
+	}
+}
